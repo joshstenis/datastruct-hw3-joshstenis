c74780422a46d13023788fcc65048ad209a18141
joshstenis <joshstenis@gmail.com>
2020-10-15 18:05:02 -0500

    Changed swap function in bubble sort to bitwise switch

--- cs2413_hw3.cpp
+++ cs2413_hw3.cpp
@@ -67,7 +67,7 @@ vector<int> bubbleSort(vector<int> v) {
     for(int i=0; i < v.size(); i++) {
         for(int j=i+1; j < v.size(); j++) {
             if(v[i] < v[j])
-                swap(v[i], v[j]);
+                v[i] = v[i] ^ v[j] ^ (v[j] = v[i]);             // Swaps values, instead of using 'swap' function
         }
     } return v;
 }
